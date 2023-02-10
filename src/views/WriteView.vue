b9c993f4ce26160db186eea4f5d1f682a748a28e
301choso <jsnn5279@naver.com>
2023-02-10 23:48:45 +0900

    first commit

---
+++ src/views/WriteView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <input type="text" placeholder="제목을 입력해주세요" />
+    <div>
+    </div>  
+    <div>
+        <textarea rows="15"></textarea>
+    </div>   
+    <button>글 작성완료</button>
+</template>
+
+<style>
+
+</style>
